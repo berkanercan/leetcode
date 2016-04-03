097d66ce89af96abf41b2c340cb061a25501d0fc
Berkan Ercan <ercanberkan@gmail.com>
2016-04-03 22:07:50 +0300

    O(n) solution.
    Previous solution was also O(n) But it was actually O(2n) ;)

--- LongestSubstringWithoutRepeatingCharacters.cpp
+++ LongestSubstringWithoutRepeatingCharacters.cpp
@@ -3,13 +3,13 @@ public:
     int lengthOfLongestSubstring(string s) {
         int longest = 0;
         int startIndex = 0;
-        int letters[128] = {0};
+        int letters[128];
+        std::fill_n(letters, 128, -1);
         for(int endIndex = 0; endIndex < s.length(); endIndex++){
-            while(letters[s[endIndex]] == 1){
-                letters[s[startIndex]] = 0;
-                startIndex++;
+            if(letters[s[endIndex]] >= startIndex){
+                startIndex = letters[s[endIndex]] + 1;
             }
-            letters[s[endIndex]] = 1;
+            letters[s[endIndex]] = endIndex;
             longest = max(endIndex - startIndex + 1,longest); //#include <algorithm>
         }
         return longest;
