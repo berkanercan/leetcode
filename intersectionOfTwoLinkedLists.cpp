1e9f50cfe684417bf0a28e84f0f665799d0f9af1
Berkan Ercan <ercanberkan@gmail.com>
2016-04-10 19:00:19 +0300

    Create intersectionOfTwoLinkedLists.cpp

---
+++ intersectionOfTwoLinkedLists.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA == NULL || headB == NULL)
+            return NULL;
+        
+        std::map<int, int> lookup;
+        
+        // put all the elements of LinkedList A into a map
+        ListNode *curNode = headB;
+        while( curNode != NULL){
+            lookup[curNode->val] = 1;
+            curNode = curNode->next;
+        }
+        
+        // now iterate through the linkedList B and compare
+        curNode = headA;
+        while( curNode != NULL){
+            if(lookup.find(curNode->val) != lookup.end() ){
+                return curNode;
+            }
+            curNode = curNode->next;
+        }
+        
+        return NULL;
+            
+    }
+};
