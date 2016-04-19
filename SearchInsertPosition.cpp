759b516bcc2738db85c1cae40234d438915abd12
Berkan Ercan <ercanberkan@gmail.com>
2016-04-19 16:04:24 +0300

    O(nlogn) time, O(1) space complexity

---
+++ SearchInsertPosition.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int begin = 0;
+        int end = nums.size() -1;
+        
+        while(begin < end){
+            int mid = (begin + end) / 2;
+            
+            if(target > nums[mid])
+                begin = mid+1;
+            else 
+                end = mid;
+        }
+        
+        if( target > nums[begin])
+            return begin+1;
+        else 
+            return begin;
+    }
+};
